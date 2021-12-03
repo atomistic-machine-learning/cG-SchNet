b2ff26d09c2ba86124e9620931b3e778ef4e6261
Niklas Gebauer <n.wa.gebauer@gmail.com>
2021-12-03 13:39:53 +0100

    removed some unused functions and imports

--- utility_classes.py
+++ utility_classes.py
@@ -4,64 +4,8 @@ import numpy as np
 import openbabel as ob
 import pybel
 from rdkit import Chem
-from rdkit.Chem.rdchem import AtomValenceException
 from multiprocessing import Process
 from scipy.spatial.distance import squareform
-#from xyz2mol import xyz2mol, ValenceError, TimeOutError
-import os
-import sys
-import ctypes
-import io
-import tempfile
-
-from contextlib import contextmanager
-
-# Adapted from
-# https://eli.thegreenplace.net/2015/redirecting-all-kinds-of-stdout-in-python/
-if (sys.platform == "win32"):
-    kernel32 = ctypes.WinDLL("kernel32")
-    # https://docs.microsoft.com/en-us/windows/console/getstdhandle
-    C_STD_ERROR_HANDLE = -12
-    c_stderr = kernel32.GetStdHandle(C_STD_ERROR_HANDLE)
-    c_flush = kernel32.FlushFileBuffers
-else:
-    libc = ctypes.CDLL(None)
-    c_stderr = ctypes.c_void_p.in_dll(libc, "stderr")
-    c_flush = libc.fflush
-
-
-@contextmanager
-def stderr_redirector(stream):
-    # The original fd stderr points to.
-    original_stderr_fd = sys.stderr.fileno()
-
-    def _redirect_stderr(to_fd):
-        """Redirect stderr to the given file descriptor."""
-        # Flush the C-level buffer stderr
-        c_flush(c_stderr)
-        # Flush and close sys.stderr - also closes the file descriptor (fd)
-        sys.stderr.close()
-        # Make original_stderr_fd point to the same file as to_fd
-        os.dup2(to_fd, original_stderr_fd)
-        # Create a new sys.stderr that points to the redirected fd
-        sys.stderr = io.TextIOWrapper(os.fdopen(original_stderr_fd, 'wb'))
-
-    # Save a copy of the original stderr fd in saved_stderr_fd
-    saved_stderr_fd = os.dup(original_stderr_fd)
-    try:
-        # Create a temporary file and redirect stderr to it
-        tfile = tempfile.TemporaryFile(mode='w+b')
-        _redirect_stderr(tfile.fileno())
-        # Yield to caller, then redirect stderr back to the saved fd
-        yield
-        _redirect_stderr(saved_stderr_fd)
-        # Copy contents of temporary file to the given stream
-        tfile.flush()
-        tfile.seek(0, io.SEEK_SET)
-        stream.write(tfile.read())
-    finally:
-        tfile.close()
-        os.close(saved_stderr_fd)
 
 
 class Molecule:
